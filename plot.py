4d60c9f1fb147861304163a72650d564b1fe8b81
Jasper Hu <jingyuanhu0626@gmail.com>
2017-02-24 13:04:54 -0500

    distribution plot

--- plot.py
+++ plot.py
@@ -10,18 +10,16 @@ def plot_distribution_tex(sample):
     ideal_2575 = emd.ideal_generator_tex('2575')
     ideal_2080 = emd.ideal_generator_tex('2080')
     bins = np.linspace(0, 1, num=31)
-
     fig, (ax0, ax1, ax2) = plt.subplots(nrows=3, figsize=(9, 9))
 
-    ax0.hist(ideal_3367, bins, normed=1, histtype='bar', facecolor='r', rwidth=0.8, alpha=0.5)
-    ax0.hist(sample, bins, normed=1, histtype='bar', facecolor='b', rwidth=0.8, alpha=0.5)
+    ax0.hist(ideal_3367, bins, normed=0, histtype='bar', facecolor='r', rwidth=0.8, alpha=0.5, weights=np.ones_like(ideal_3367)/float(len(ideal_3367)))
+    ax0.hist(sample, bins, normed=0, histtype='bar', facecolor='b', rwidth=0.8, alpha=0.5, weights=np.ones_like(sample)/float(len(sample)))
 
-    # Create a histogram by providing the bin edges (unequally spaced).
-    ax1.hist(ideal_2575, bins, normed=1, histtype='bar', facecolor='r', rwidth=0.8, alpha=0.5)
-    ax1.hist(sample, bins, normed=1, histtype='bar', facecolor='b', rwidth=0.8, alpha=0.5)
+    ax1.hist(ideal_2575, bins, normed=0, histtype='bar', facecolor='r', rwidth=0.8, alpha=0.5, weights=np.ones_like(ideal_2575)/float(len(ideal_2575)))
+    ax1.hist(sample, bins, normed=0, histtype='bar', facecolor='b', rwidth=0.8, alpha=0.5, weights=np.ones_like(sample)/float(len(sample)))
 
-    ax2.hist(ideal_2080, bins, normed=1, histtype='bar', facecolor='r', rwidth=0.8, alpha=0.5)
-    ax2.hist(sample, bins, normed=1, histtype='bar', facecolor='b', rwidth=0.8, alpha=0.5)
+    ax2.hist(ideal_2080, bins, normed=0, histtype='bar', facecolor='r', rwidth=0.8, alpha=0.5, weights=np.ones_like(ideal_2080)/float(len(ideal_2080)))
+    ax2.hist(sample, bins, normed=0, histtype='bar', facecolor='b', rwidth=0.8, alpha=0.5, weights=np.ones_like(sample)/float(len(sample)))
 
     fig.tight_layout()
     plt.show()
